8a713e188842f4b53b9334ab226213f8defeda45
mhtr <reportkarnahai1@gmail.com>
2025-12-28 16:56:57 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,5 @@
+class Hello{
+    public static void main(Hiii[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Hiii.java
@@ -0,0 +1,27 @@
+class Hiii {
+   public static void main(String[] var0) {
+   //  String s = "Hello";
+   //  String s1 =s;
+   //  System.out.println(s);
+   //  s = s.concat("Hiii");
+   //  s = "hey";
+   //  System.out.println(s +"bca");
+   //  System.out.println(s1);
+
+      String name = " Rajiv";
+      String tittle = "Ranjan";
+
+      // System.out.println(name);
+      // System.out.println(tittle.length());
+      // System.out.println(name.charAt(3));
+      // System.out.println(name.substring(3));
+      // System.out.println(name.equals(tittle));
+      // System.out.println(name.equalsIgnoreCase(tittle));
+      // System.out.println(name.toUpperCase());
+      // System.out.println(name.trim());
+
+
+
+
+   }
+}
