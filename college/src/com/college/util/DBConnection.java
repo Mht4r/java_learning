6c97eddca07f7d631cb1f233ec3221e76b4e1770
mhtr <reportkarnahai1@gmail.com>
2026-01-26 00:39:48 +0530

    Update JDBC config and fixes

--- college/src/com/college/util/DBConnection.java
+++ college/src/com/college/util/DBConnection.java
@@ -2,12 +2,14 @@ package com.college.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBConnection {
 
-    private static final String URL = System.getenv().getOrDefault("DB_URL", "jdbc:mysql://localhost:3306/college");
-    private static final String USER = System.getenv().getOrDefault("DB_USER", "root");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+    private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    private static final String URL = dotenv.get("DB_URL", "jdbc:mysql://localhost:3306/college");
+    private static final String USER = dotenv.get("DB_USER", "root");
+    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASSWORD);
